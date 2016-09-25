71f74d5e65c905acebda6f5c564bc048ea0bdeeb
joe-re <joe.tialtngo@gmail.com>
2016-09-25 22:00:01 +0900

    add component command

---
+++ templates/components/Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default <%= input %> extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1><%= input %> Component!</h1>
+      </div>
+    );
+  }
+}
